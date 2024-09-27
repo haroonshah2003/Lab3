c80d7514ae86965927846291102594e11c8e4f93
Haroon Shah <haroon.shah@mail.utoronto.ca>
2024-09-27 10:54:00 -0400

    done

--- src/main/java/org/translation/JSONDemo.java
+++ src/main/java/org/translation/JSONDemo.java
@@ -33,8 +33,14 @@ public class JSONDemo {
      * @return value of key "key1" from the second object in the given jsonArray
      */
     public static String getKeyOneOfSecond(JSONArray jsonArray) {
-        // TODO: Complete this method.
-        return "";
+        String a = "";
+        for (int i = 1; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String key = jsonObject.getString("key1");
+            if (key.equals("string1b")) {
+                a = key;
+            }
+        }
+        return a;
     }
-
 }
